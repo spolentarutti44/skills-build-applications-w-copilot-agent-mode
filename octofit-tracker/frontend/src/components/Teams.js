5a351ca7fb990e131596d1846363794a15da3fc4
spolentarutti44 <47154850+spolentarutti44@users.noreply.github.com>
2025-04-08 20:18:42 +0000

    Add Activities component for displaying activities fetched from API

---
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('https://symmetrical-guacamole-4wv56wpwxxjh797p-8000.app.github.dev/api/teams/')
+      .then(response => response.json())
+      .then(data => setTeams(data))
+      .catch(error => console.error('Error fetching teams:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-center">Teams</h1>
+      <ul className="list-group">
+        {teams.map(team => (
+          <li key={team._id} className="list-group-item">{team.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Teams;
